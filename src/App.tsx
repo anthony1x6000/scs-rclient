6ba423b56a1b6a8fb72a46d157827391db8d13d1
Anthony Stevens <33004321+anthony1x6000@users.noreply.github.com>
2026-06-19 14:13:40 -0400

    feat: UI shell complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import BaseWebDAVURL from "./components/BaseWebDAVUrl";
 function App() {
   return (
     <BackgroundWrapper>
-      <div className="p-[2%] font-['Roboto'] font-light">
-        <Dropdown />
-        a subdirectory of your WebDAV drive
-      </div>
 
       <div className="bottom-0 absolute p-[2%] w-[100%] text-white">
+        <div className="p-[2%] font-['Roboto'] font-light">
+          <div className="text-nowrap">
+          <Dropdown />
+          <div className="inline italic">
+            a subdirectory of your WebDAV drive
+          </div>
+          </div>
+        </div>
         <BaseWebDAVURL />
       </div>
     </BackgroundWrapper>
