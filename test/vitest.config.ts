9cf43a6f9c5899f8443f2915170bd9d7130a5ad5
Anthony Stevens <33004321+anthony1x6000@users.noreply.github.com>
2026-07-08 13:37:20 -0400

    Update vitest.config.ts

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -2,7 +2,9 @@ import { defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from '../vite.config';
 
 export default defineConfig(async (env) => {
-  const resolvedViteConfig = await (typeof viteConfig === 'function' ? viteConfig(env) : viteConfig);
+  // @ts-ignore
+  const config = viteConfig.default || viteConfig;
+  const resolvedViteConfig = await (typeof config === 'function' ? config(env) : config);
   return mergeConfig(resolvedViteConfig, {
     test: {
       environment: 'jsdom',
