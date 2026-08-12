e0c04ca68264e7d8bc41e7253915c513ec27db4e
Anthony Stevens <33004321+anthony1x6000@users.noreply.github.com>
2026-08-12 11:41:57 -0400

    chore: update packages

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,10 +1,19 @@
 module.exports = {
   hooks: {
     readPackage(pkg) {
-      if (pkg.dependencies && pkg.dependencies.esbuild) {
-        pkg.dependencies.esbuild = "^0.28.1";
+      if (pkg.dependencies) {
+        if (pkg.dependencies.esbuild) {
+          pkg.dependencies.esbuild = "^0.28.1";
+        }
+        if (pkg.dependencies.postcss) {
+          pkg.dependencies.postcss = "^8.5.26";
+        }
+        if (pkg.dependencies.nanoid) {
+          pkg.dependencies.nanoid = "^3.3.18";
+        }
       }
       return pkg;
     },
   },
 };
+
